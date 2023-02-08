16967298ff60f7a58146ec7a1e2453232b60c125
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2023-02-08 15:37:42 +0530

    frist cose

---
+++ Untitled-1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // vector<int>v(100,9);
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<endl;
+    // }
+
+    int arr[100]={};
+
+    for(int i=0; i<100;i++)
+    {
+        cout<<arr[i];
+    }
+
+
+
+
+    return 0;
+}
